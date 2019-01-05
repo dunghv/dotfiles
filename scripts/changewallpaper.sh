3911dd33924ce9cbe489c830f4afae2ca42be7c8
Hoang Viet Dzung <dunghv@coccoc.com>
2019-01-05 18:01:14 +0700

    change theme

--- scripts/changewallpaper.sh
+++
@@ -1,6 +0,0 @@
-#/bin/bash
-
-wget -O ~/Pictures/wallpaper.jpg https://unsplash.it/1920/1080/?random
-#wget -O /tmp/wallpaper.jpg https://source.unsplash.com/collection/1065396/1920x1080
-#wget -O /tmp/wallpaper.jpg https://source.unsplash.com/user/dunghv/likes
-gsettings set org.gnome.desktop.background picture-uri file:///home/dunghv/Pictures/wallpaper.jpg
